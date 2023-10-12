788f9a89181331c1bbfb4e2f7cd6ee2e61efc278
Vitalie <138406664+Vitalie-Dragan@users.noreply.github.com>
2023-10-12 12:23:41 +0100

    Check the type of concert

--- concert.rb
+++ concert.rb
@@ -26,6 +26,31 @@ class Concert
   end
 
 
+  # 1   Create a method with Concert/ Livesream/ Festival
+  # 2   Create 2 test to check if is's vallid or invalid
+  # 3    Create start_date method 
+  #  
+
+
+  def type
+    @type
+  end
+
+  def type_is_valid
+    return VALID_TYPES.include?(@type)
+  end
+  
+  def headliners
+    if @type == 'concert' || @type == 'livestream'
+      return @artists[0]
+    elsif @type == 'festival'
+      return @artists
+    end
+  end
+
+  
+
+
 end
 
 
--- spec/concert_spec.rb
+++ spec/concert_spec.rb
@@ -3,8 +3,13 @@ require './concert'
 describe 'Concert' do
 
   let(:valid_concert)   { Concert.new(['artist1',  'artist2'], 100, '2023/11/11', 'concert') }
-  let(:invalid_concert) { Concert.new([], 100, '2023/11/11', 'concert') }
+  let(:invalid_concert) { Concert.new([], 100, '2023/11/11', 'sdagasgd') }
   
+  let(:concert)    { Concert.new(['ab', 'ac', 'ad', 'bcd'], 100, '2023/11/11', 'concert') }
+  let(:livestream) { Concert.new(['ab', 'ac', 'ad', 'bcd'], 100, '2023/11/11', 'livestream') }
+  let(:festival)   { Concert.new(['ab', 'ac', 'ad', 'bcd'], 100, '2023/11/11', 'festival') }
+
+
 
 
   describe "#Validation" do
@@ -49,6 +54,61 @@ describe 'Concert' do
     end
   end
 
+  
+# My Code
+
+  # type method 
+  describe '#type' do
+    context 'when type is concert' do
+      it 'returns the event type' do
+        expect(valid_concert.type).to eq('concert')
+      end
+    end
+
+
+
+    context 'when type is livestream' do
+      before{allow(valid_concert).to receive(:type).and_return('livestream')}
+      it 'returns true' do
+        
+        expect(valid_concert.type_is_valid).to eq(true)
+      end
+    end
+    context 'when type is festival' do
+      before{allow(valid_concert).to receive(:type).and_return('festival')}
+      it 'returns true' do
+        expect(valid_concert.type_is_valid).to eq(true)
+      end
+    end
+  end
+
+  # type_is_valid method 
+  describe 'type_is_valid' do
+    context 'when type is neither concert nor livestream' do
+      before{allow(invalid_concert).to receive(:type).and_return('comedy')}
+      it 'returns false' do
+        expect(invalid_concert.type_is_valid).to eq(false)
+      end
+    end
+  end
+
+  # headliners
+  describe 'headliners' do
+    context 'when concert or livestream' do
+      it 'returns first artist in array' do
+        expect(concert.headliners).to eq('ab')
+        expect(livestream.headliners).to eq('ab')
+      end
+    end
+    context 'when fetstival' do
+      it 'returns entire array' do
+        expect(festival.headliners).to eq(['ab', 'ac', 'ad', 'bcd'])
+      end
+    end
+  end
+
+  # satrt date
+  
 
   
 end
